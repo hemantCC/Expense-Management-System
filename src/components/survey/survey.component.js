ac1efd44210142a0fdb1c5b5229d6d33caeaddbe
hemantCC <49364193+hemantCC@users.noreply.github.com>
2020-09-18 23:59:15 +0530

    updates

--- src/components/survey/survey.component.js
+++ src/components/survey/survey.component.js
@@ -1,55 +1,204 @@
-import { Button } from "@material-ui/core";
+import { Button, Divider, TextField } from "@material-ui/core";
 import React, { Component } from "react";
 import Data from "../../assets/files/surveyData.json";
 import Header from "../shared/header.component";
+import jspdf from "jspdf";
 
 class SurveyComponent extends Component {
-  state = {
-    takeSurvey: true,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      takeSurvey: false,
+    };
+  }
 
   handleTakeSurvey = () => {
     this.setState({
       takeSurvey: true,
+      formError: "",
     });
   };
+
+  handleSubmit = () => {
+    if (this.formError === "") {
+      var answers = this.state;
+      delete answers.takeSurvey;
+      const questions = Data.map((question) => {
+        return question.question;
+      });
+      console.log(questions);
+      console.log(answers);
+      this.generatePdf(questions, answers);
+      Data.map((question) => {
+        return {
+          ...this.setState([question.id]),
+        };
+      });
+    } else {
+      console.log("invalid");
+    }
+  };
+
+  generatePdf = (question, answers) => {
+    var doc = new jspdf("p", "pt");
+
+    doc.text(20, 13, "User Survey");
+
+    for (var i = 0; i < question.length; i++) {
+      doc.text(20, 20 * (i + 2), "Question" + (i + 1) + " :" + question[i]);
+    }
+    for (var i = 1; i <= question.length; i++) {
+      doc.text(20, 20 * (i + 8), "Answer" + i + " : " + answers[i]?.toString());
+    }
+    doc.save("MySurvey.pdf");
+  };
+
   render() {
     return (
       <div>
         <Header />
         <div className="row mx-0 justify-content-center">
           <div className="col-md-11 cat-section mt-4 survey">
-            <div className="display-4  mb-4  text-center">Survey</div>
-            {!this.state.takeSurvey && (
-              <button
-                className="btn btn-success"
-                onClick={this.handleTakeSurvey}
-              >
-                Click here to Take Survey
-              </button>
-            )}
+            <div className="display-4  mb-4  text-center">
+              Survey
+              <p>
+                {!this.state.takeSurvey && (
+                  <button
+                    className="btn btn-success"
+                    onClick={this.handleTakeSurvey}
+                  >
+                    Click here to Take Survey
+                  </button>
+                )}
+              </p>
+            </div>
             {this.state.takeSurvey &&
               Data.map((question) => {
                 return (
-                  <div key={question.id}>
-                    <p>
+                  <div key={question.id} className="ml-5 mt-3">
+                    <div>
                       {question.id}. {question.question}
-                    </p>
-                    <p>
+                      {question.required && (
+                        <span style={{ color: "red" }}>*</span>
+                      )}
+                      {question.required && (
+                        <i
+                          className="fa fa-info-circle ml-4"
+                          data-toggle="tooltip"
+                          data-placement="right"
+                          title="Answer is Required!"
+                          style={{ fontSize: 23 }}
+                        ></i>
+                      )}
+                    </div>
+                    <div>
                       answer:
+                      {question.questionType === "text" && (
+                        <span className="form-group ml-2">
+                          <TextField
+                            placeholder="write your answer here"
+                            className="col-md-4 mb-3"
+                            value={this.state[question.id]}
+                            onChange={(event) =>
+                              this.setState({
+                                [question.id]: event.target.value,
+                              })
+                            }
+                          />
+                        </span>
+                      )}
+                      {question.questionType === "amount" && (
+                        <span className="form-group ml-2">
+                          <TextField
+                            type="number"
+                            placeholder="write the amount here"
+                            className="col-md-4 mb-3"
+                            value={this.state[question.id]}
+                            onChange={(event) =>
+                              this.setState({
+                                [question.id]: event.target.value,
+                              })
+                            }
+                          />
+                        </span>
+                      )}
+                      {question.questionType === "textarea" && (
+                        <span className="form-group ml-2">
+                          <textarea
+                            className="col-md-4 mb-3"
+                            placeholder="write your answer here"
+                            rows="4"
+                            value={this.state[question.id]}
+                            onChange={(event) =>
+                              this.setState({
+                                [question.id]: event.target.value,
+                              })
+                            }
+                          ></textarea>
+                        </span>
+                      )}
+                      {question.questionType === "boolean" && (
+                        <span className="form-group ml-2 mb-3">
+                          <div className="form-check">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="exampleRadios5"
+                              id="exampleRadios5"
+                              value={this.state[question.id]}
+                              onChange={(event) =>
+                                this.setState({
+                                  [question.id]: true,
+                                })
+                              }
+                            />
+                            <label
+                              className="form-check-label"
+                              htmlFor="exampleRadios5"
+                            >
+                              True
+                            </label>
+                          </div>
+                          <div className="form-check">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="exampleRadios5"
+                              id="exampleRadios6"
+                              value={this.state[question.id]}
+                              onChange={(event) =>
+                                this.setState({
+                                  [question.id]: false,
+                                })
+                              }
+                            />
+                            <label
+                              className="form-check-label"
+                              htmlFor="exampleRadios6"
+                            >
+                              False
+                            </label>
+                          </div>
+                        </span>
+                      )}
                       {question.questionType === "mcq" ? (
-                        <div>
+                        <div className=" mb-3">
                           <div className="form-check">
                             <input
                               className="form-check-input"
                               type="radio"
                               name="exampleRadios"
                               id="exampleRadios1"
-                              value={question.option1}
+                              value={this.state[question.id]}
+                              onChange={(event) =>
+                                this.setState({
+                                  [question.id]: question.option1,
+                                })
+                              }
                             />
                             <label
                               className="form-check-label"
-                              for="exampleRadios1"
+                              htmlFor="exampleRadios1"
                             >
                               {question.option1}
                             </label>
@@ -60,11 +209,16 @@ class SurveyComponent extends Component {
                               type="radio"
                               name="exampleRadios"
                               id="exampleRadios2"
-                              value={question.option2}
+                              value={this.state[question.id]}
+                              onChange={(event) =>
+                                this.setState({
+                                  [question.id]: question.option2,
+                                })
+                              }
                             />
                             <label
                               className="form-check-label"
-                              for="exampleRadios2"
+                              htmlFor="exampleRadios2"
                             >
                               {question.option2}
                             </label>
@@ -75,11 +229,16 @@ class SurveyComponent extends Component {
                               type="radio"
                               name="exampleRadios"
                               id="exampleRadios3"
-                              value={question.option3}
+                              value={this.state[question.id]}
+                              onChange={(event) =>
+                                this.setState({
+                                  [question.id]: question.option3,
+                                })
+                              }
                             />
                             <label
                               className="form-check-label"
-                              for="exampleRadios3"
+                              htmlFor="exampleRadios3"
                             >
                               {question.option3}
                             </label>
@@ -90,11 +249,16 @@ class SurveyComponent extends Component {
                               type="radio"
                               name="exampleRadios"
                               id="exampleRadios4"
-                              value={question.option4}
+                              value={this.state[question.id]}
+                              onChange={(event) =>
+                                this.setState({
+                                  [question.id]: question.option4,
+                                })
+                              }
                             />
                             <label
                               className="form-check-label"
-                              for="exampleRadios4"
+                              htmlFor="exampleRadios4"
                             >
                               {question.option4}
                             </label>
@@ -103,12 +267,18 @@ class SurveyComponent extends Component {
                       ) : (
                         ""
                       )}
-                    </p>
+                    </div>
+                    <Divider />
                   </div>
                 );
               })}
             {this.state.takeSurvey && (
-              <Button variant="contained" color="primary">
+              <Button
+                variant="contained"
+                color="primary"
+                className="ml-5"
+                onClick={this.handleSubmit}
+              >
                 Submit Survey
               </Button>
             )}
